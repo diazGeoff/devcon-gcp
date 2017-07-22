d5eaac7803c3d0412316d7eaa057fc1b228ab901
Geoff Diaz <diaz.geoff.work@gmail.com>
2017-07-22 10:35:55 +0800

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+Devcon - GCP 
+===============
+A sample nodejs project deployed for Google Cloud Platform
+
+How to run
+===============
+	npm install
+	npm start
---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname+ "/assets"));
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/views/test.html");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Port running on: ${PORT}`);
+});
